5618887305f8956a6b01e23b2c8bfad05355e7f2
Namit Kumar <namitkumar2208@gmail.com>
2024-12-19 00:00:29 +0530

    fix login page

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -91,14 +91,13 @@ const LoginSignup = () => {
       setError(error.response?.data?.message || "An error occurred");
     }
   };
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900 bg-check-pattern">
       <div className="w-full max-w-5xl bg-white rounded-xl shadow-xl overflow-hidden flex transform transition duration-300 hover:scale-105">
         {/* Left Panel */}
         <div className="w-1/2 bg-gradient-to-br from-purple-600 to-indigo-700 text-white p-8 flex flex-col items-center justify-center">
           <h2 className="text-5xl font-extrabold mb-6 animate-fadeIn">
-            {isSignup ? "Register" : "Login"}
+            {isSignup ? "Sign Up" : "Sign In"}
           </h2>
           <p className="text-lg mb-8 text-center px-6 animate-fadeIn">
             {isSignup
@@ -109,7 +108,7 @@ const LoginSignup = () => {
             onClick={() => setIsSignup(!isSignup)}
             className="bg-white text-purple-600 px-8 py-2 rounded-full font-bold hover:bg-gray-200 shadow-md animate-bounce"
           >
-            {isSignup ? "LOGIN" : "REGISTER"}
+            {isSignup ? "SIGN IN" : "REGISTER"}
           </button>
         </div>
 
